51b85690d9099f40da1b8e73ae3b81ccd32b3240
DESKTOP-LKHLQTC\artis <318802657@qq.com>
2026-08-19 00:10:51 +0800

    feat: launch Niwde technical blog

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Niwde';
+export const SITE_DESCRIPTION = '一个游戏后端开发者的 AI 实践与技术笔记。';
+export const SITE_URL = 'https://yangjiexin.com';
+export const GITHUB_URL = 'https://github.com/lvivvde';
+export const REALMMESH_URL = 'https://github.com/lvivvde/RealmMesh';
